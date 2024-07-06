cae59aa033ebe7dbda615ba69114bc647159bcbe
philippr1212 <Philipp.Rinau@myhsba.de>
2024-07-06 21:02:47 +0200

    Revert "Splash function (have to be repaired)"
    This reverts commit 47a8e38c157ab388965b811466a5f6d9c10cc141.

--- app/src/main/java/com/example/newfoodme/MainActivity.kt
+++ app/src/main/java/com/example/newfoodme/MainActivity.kt
@@ -11,21 +11,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.newfoodme.ui.theme.NewFoodMeTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().setKeepOnScreenCondition { true }
+        enableEdgeToEdge()
         setContent {
-            AnimatedSplashTheme {
+            NewFoodMeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
+}
 
-    private fun AnimatedSplashTheme(function: () -> Unit) {
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewFoodMeTheme {
+        Greeting("Android")
     }
 }
